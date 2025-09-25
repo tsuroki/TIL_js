78e003d19dca79be57df661c30839bb3eda4566f
tsuroki <tsuroki0210@gmail.com>
2025-09-25 10:19:19 +0900

    1교시

--- task/script/flower_date.js
+++ task/script/flower_date.js
@@ -3,49 +3,61 @@ const birthday_flower = [
         month:1,
         flower:'수선화',
         content:'자기애, 자존심, 외로움',
+        src : 'https://i.pinimg.com/736x/50/30/5e/50305e5f6f94704532d1dff5140237a8.jpg'
     },{
         month:2,
         flower:'제비꽃',
         content:'겸손, 양보',
+        src : 'https://i.pinimg.com/736x/97/86/34/978634371345e7e8fdaf8f3b4fd856f7.jpg'
     },{
         month:3,
         flower:'수선화',
         content:'자기애, 자존심, 외로움',
+        src : 'https://i.pinimg.com/736x/50/30/5e/50305e5f6f94704532d1dff5140237a8.jpg'
     },{
         month:4,
         flower:'스위트피',
-        content:'추억, 즐거움'
+        content:'추억, 즐거움',
+        src : 'https://i.pinimg.com/1200x/9b/d6/5d/9bd65da0b86c9c80c732a11fb353b205.jpg'
     },{
         month:5,
         flower:'은방울꽃',
-        content:'희망, 섬세함'
+        content:'희망, 섬세함',
+        src : 'https://i.pinimg.com/736x/e9/45/f1/e945f17906933c4d273afb36f27f9ac9.jpg'
     },{
         month:6,
         flower:'백합',
-        content:'순결'
+        content:'순결',
+        src : 'https://i.pinimg.com/736x/c3/6b/f2/c36bf2d3bb6aea818f517d5a085618aa.jpg'
     },{
         month:7,
         flower:'미나리아재비',
-        content:'아름다움, 인격'
+        content:'아름다움, 인격',
+        src : 'https://i.pinimg.com/736x/9a/fc/49/9afc49ef8e052fe313475f02a702ccd1.jpg'
     },{
         month:8,
         flower:'글라디올러스',
-        content:'비밀, 상상, 견고함'
+        content:'비밀, 상상, 견고함',
+        src : 'https://i.pinimg.com/1200x/d2/c3/3e/d2c33eeaba75b2a6edc56415a46e27dc.jpg'
     },{
         month:9,
         flower:'물망초',
-        content:'진실한 사랑'
+        content:'진실한 사랑',
+        src : 'https://i.pinimg.com/1200x/8f/6b/7a/8f6b7a294f1c0a65b56eabf201370422.jpg'
     },{
         month:10,
         flower:'금잔화',
-        content:'실망, 비애'
+        content:'실망, 비애',
+        src : 'https://i.pinimg.com/736x/64/c3/21/64c3218138622bd6c9accce8e81d9937.jpg'
     },{
         month:11,
         flower:'국화',
-        content:'성실, 진실'
+        content:'성실, 진실',
+        src : 'https://i.pinimg.com/736x/1f/14/a0/1f14a024fda8cdb272135999784507d4.jpg'
     },{
         month:12,
         flower:'포인세티아',
-        content:'축하, 감사'
+        content:'축하, 감사',
+        src : 'https://i.pinimg.com/736x/05/99/3f/05993f43fc71c36efaca0a8db1dadb9b.jpg'
     }
 ]
